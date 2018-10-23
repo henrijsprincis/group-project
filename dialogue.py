56e36a8669ee925b413676968656d6b2c8236324
matth2000 <44266937+matth2000@users.noreply.github.com>
2018-10-23 14:07:13 +0100

    update

--- dialogue.py
+++ dialogue.py
@@ -26,10 +26,41 @@ dialogue_shower = {
     "numbers": {"1": "dialogue_shower", "2": "dialogue_no_shower"}
 }
 
+dialogue_phone = {
+    "name": "Reach for Phone",
+    "description":"Reach your phone and start scrolling and You discover photos from the other night. Are those your study notes on the table at pre drinks?,Call Jason to find out what happened He tells you that you left your notes at pre drinks and they are still at his flat in Taly South. You feel exhausted but you need to get up.",
+    "numbers": {"1": "dialogue_kitchen", "2": "dialogue_bathroom", "3":"dialogue_jason_flat_through"}
+}
+
+dialogue_jason_flat_through = {
+    "name": "Bump Housemate",
+    "description":"Throw on your clothes and leave to Jason’s flat. On your way out you bump into your housemate.You find out denise is at the library studying.",
+    "numbers": {"1": "dailogue_jason", "2": "dialogue_library"}
+}
+
+dialogue_coffee = {
+    "name": "Make Coffee",
+    "description":"This wiil boost your energy",
+    "numbers": {"1": "dialogue_bathroom", "2": "dialogue_jason", "3": "dialogue_library"}
+    "items":[items_coffee]
+}
+
+dialogue_breakfast = {
+    "name": "Make Breakfast",
+    "description":"You find a Python book in the cereal box. What a coincidence.",
+    "numbers": {"1": "take_book", "2": "leave_book"}
+    "items":[items_book]
+}
+
 
 dialogue = {
     "dialogue_start": dialogue_start,
     "dialogue_kitchen": dialogue_kitchen,
     "dialogue_bathroom": dialogue_bathroom,
     "dialogue_shower": dialogue_shower,
+    "dialogue_phone":dialogue_phone,
+    "dialogue_jason_flat_through": dialogue_jason_flat_through,
+    "dialogue_coffee": dialogue_coffee,
+    "dialogue_breakfast": dialogue_breakfast
+    
 }
