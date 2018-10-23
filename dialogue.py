6dd2cbfa9fe17bd291217b3d1a5d63cea17c3e09
matth2000 <44266937+matth2000@users.noreply.github.com>
2018-10-23 14:57:19 +0100

    final update

--- dialogue.py
+++ dialogue.py
@@ -5,51 +5,94 @@ dialogue_start = {
     "description":
     """You wake up and a long night of heavy drinking is catching up on you. Have you just dreamt about Kirill? Confused, you look for something to hold on to. Your stomach is rambling, and a massive headache has made its appearance. Wait, are you even alive? More confused, you look through the window and you are kind of proud that you woke up early for once. It must be Kirill. """,
     "numbers":{"1": "dialogue_kitchen", "2": "dialogue_phone", "3": "dialogue_bathroom"},
+    "time" : 0,
     "items": []
 }
 
 dialogue_kitchen = {
     "name": "Kitchen",
     "description":"You Go to the kitchen,  you need something to wake you up .  You bump into your housemate.  You have quick chat about what happened last night.  He tells you that you were studying at Jason’s flat and Denise called round in the morning to say she’s studying at the library.",
-    "numbers": {"1": "dialogue_breakfast", "2": "dialogue_coffee"}
+    "time":5,
+    "numbers": {"1": "dialogue_breakfast", "2": "dialogue_coffee"},
+    "items": []
 }
 
 dialogue_bathroom = {
     "name": "Bathrom",
     "description":"You smell and you definitely need a shower",
-    "numbers": {"1":"dialogue_shower", "2":"dialogue_no_shower"}
-}
-
-dialogue_shower = {
-    "name": "Bathrom",
-    "description":"You smell and you definitely need a shower",
-    "numbers": {"1": "dialogue_shower", "2": "dialogue_no_shower"}
+    "numbers": {"1":"dialogue_shower", "2":"dialogue_no_shower"},
+    "items": []
 }
 
 dialogue_phone = {
     "name": "Reach for Phone",
     "description":"Reach your phone and start scrolling and You discover photos from the other night. Are those your study notes on the table at pre drinks?,Call Jason to find out what happened He tells you that you left your notes at pre drinks and they are still at his flat in Taly South. You feel exhausted but you need to get up.",
-    "numbers": {"1": "dialogue_kitchen", "2": "dialogue_bathroom", "3":"dialogue_jason_flat_through"}
+    "time": 10,
+    "numbers": {"1": "dialogue_kitchen", "2": "dialogue_bathroom", "3":"dialogue_jason_flat_through"},
+    "items": []
+    
 }
 
 dialogue_jason_flat_through = {
     "name": "Bump Housemate",
     "description":"Throw on your clothes and leave to Jason’s flat. On your way out you bump into your housemate.You find out denise is at the library studying.",
-    "numbers": {"1": "dailogue_jason", "2": "dialogue_library"}
+    "time": 5,
+    "numbers": {"1": "dailogue_jason", "2": "dialogue_library"},
+    "items": []
 }
 
 dialogue_coffee = {
     "name": "Make Coffee",
     "description":"This wiil boost your energy",
-    "numbers": {"1": "dialogue_bathroom", "2": "dialogue_jason", "3": "dialogue_library"}
+    "time": 5,
+    "numbers": {"1": "dialogue_bathroom", "2": "dialogue_jason", "3": "dialogue_library"},
     "items":[items_coffee]
 }
 
 dialogue_breakfast = {
     "name": "Make Breakfast",
     "description":"You find a Python book in the cereal box. What a coincidence.",
-    "numbers": {"1": "take_book", "2": "leave_book"}
+    "numbers": {"1": "take_book", "2": "leave_book"},
+    "time": 15,
     "items":[items_book]
+}
+
+dialogue_jason = {
+    "name": "Jason's Flat"
+    "description" : "You go to Jsons flat and find there is no answr when you knock on the door"
+    "numbers":{"1":"dialogue_wait", "2":"dialogue_library"}
+    "time":20
+    "items": []
+}
+
+dialogue_wait = {
+    "name": "Wait",
+    "description" : "You wait and knock again after 5 minutes and the door is answered, you enter the house and see your notes",
+    "numbers":{"1":"take_notes", "2":"dialogue_stay", "3":"dialogue_library"},
+    "time":5,
+    "items": [items_notes]
+}
+
+dialogue_stay = {
+    "name": "Stay",
+    "description" : "You wait and knock again after 5 minutes and the door is answered, you enter the house and see your notes",
+    "numbers":{"1":"dialogue_exam"},
+    "time":60,
+    "items": []
+}
+
+dialogue_library = {
+    "name":"Library",
+    "description":"You meet Denise in the library and study for half an hour soon your stomach is rumbling and energy is low",
+    "numbers":{"1":"dialogue_food", "2":"dialogue_no_food"},
+    "items": []
+}
+
+dialogue_food = {
+    "name":"Hallway",
+    "description":"On your way from getting food yu notice a key on the floor",
+    "numbers":{"1":"dialogue_investigate", "2":"dialogue_library"},
+    "items": []
 }
 
 
@@ -62,5 +105,9 @@ dialogue = {
     "dialogue_jason_flat_through": dialogue_jason_flat_through,
     "dialogue_coffee": dialogue_coffee,
     "dialogue_breakfast": dialogue_breakfast
-    
+    "dialogue_library": dialogue_library
+    "dialogue_wait": dialogue_wait
+    "dialogue_stay": dialogue_stay
+    "dialogue_jason": dialogue_jason
+    "
 }
