3a1dec6ba9eb384133915dd5525c10079c0676b4
matth2000 <HermanM@cardiff.ac.uk>
2018-10-24 11:00:19 +0100

    test file update

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 num1 = {"ans" : "A", "ques" : "1. Which is a valid variable name in python?", "choice": "\n A. answer1 \n B. 32test \n C. while \n D. vari!£"} 
-num3 = {"ans" : "B", "ques" : "3. What is the correct order of precedence (Asending order", "choice": "\n A. ** % + \n B. ** % + \n C. % () + \n D. () + %"}
+num2 = {"ans" : "D", "ques" : "1. What keyword is used to print a value to the screen?", "choice": "\n A. show \n B. output \n C. write \n D. print"} 
+num3 = {"ans" : "B", "ques" : "3. What is the correct order of precedence (Asending order)?", "choice": "\n A. ** % + \n B. ** % + \n C. % () + \n D. () + %"}
 num4 = {"ans" : "A", "ques" : "4. What is the result of 5%3", "choice": "\n A. 2 \n B. 3 \n C. 5 \n D. 7"}
 num5 = {"ans" : "C", "ques" : "5. What is the result of (10 + (21 ÷ 3)) * 3", "choice": "\n A. 40 \n B. 10 \n C. 51 \n D. 75"}
 num6 = {"ans" : "D", "ques" : "6. What is the data type of the value True", "choice": "\n A. Float \n B. Integer \n C. String \n D. Boolean"}
@@ -7,5 +8,10 @@ num7 = {"ans" : "B", "ques" : "7. What is the result of 8+15.5", "choice": "\n A
 num8 = {"ans" : "B", "ques" : "8. What is the Output of apple.count('p')", "choice": "\n A. 1 \n B. 2 \n C. p \n D. 5"}
 num9 = {"ans" : "C", "ques" : "9. What word is used to read a module dile and create a module object", "choice": "\n A. call \n B. create module \n C. import \n D. module()"}
 num10 = {"ans" : "A", "ques" : "10. What is the result when y : 3 and x : 1 \n x > y ", "choice": "\n A. False \n B. True \n C. ERROR \n D. No result"}
+num11 = {"ans" : "C", "ques" : "11. Which keyword is used to start a function in Python?", "choice": "\n A. import \n B. start \n C. def \n D.  function"}
+num12 = {"ans" : "B", "ques" : "12. What does this operator do? \n ** ", "choice": "\n A. Modulus \n B. Exponent \n C. Division \n D. Multiplication"} 
+num13 = {"ans" : "D", "ques" : "13. What is the comparison operator for showing if a value is not equal to?", "choice": "\n A. => \n B. > \n C. == \n D. !="} 
+num14 = {"ans" : "D", "ques" : "14. What statement gets a value from the command line?", "choice": "\n A. take \n B. command \n C. << \n D. input()"} 
+num15 = {"ans" : "B", "ques" : "15. What would this statement output? \n print len('abc') ", "choice": "\n A. abc \n B. 3 \n C. Error \n D. len('abc')"}
 
-nums = {"1":num1, "3":num3, "4":num4, "5":num5, "6":num6, "7":num7, "8":num8, "9":num9, "10":num10}   
+nums = {"1":num1, "2": num2, "3":num3, "4":num4, "5":num5, "6":num6, "7":num7, "8":num8, "9":num9, "10":num10, "11":num11, "12":num12, "13":num13, "14":num14, "15":num15}   
